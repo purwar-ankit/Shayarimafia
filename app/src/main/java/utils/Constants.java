14d5ee609c68186b8a342b6010ee85624bbbdb42
unknown <ankit.purwar@Ankit-Purwar.diasparkindia.com>
2016-06-08 19:51:17 +0530

    added AppController,Constants,LruBitmapCache classes

---
+++ app/src/main/java/utils/Constants.java
@@ -0,0 +1,14 @@
+package utils;
+
+
+/**
+ * Created by ankit.purwar on 6/8/2016.
+ */
+
+public class Constants {
+
+    public static String TAG = "ShayariMafia";
+    public static String categotiesUrl = "http://";
+    public static String shayariOfCatUrl = "http://";
+
+}
